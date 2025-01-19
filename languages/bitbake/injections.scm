c77064e56f271181d6140d2ac5a81311b985f6d7
Mikhail Anikin <anikin.mikhail.d@gmail.com>
2025-01-19 10:08:44 +0200

    Fix injections schema

--- languages/bitbake/injections.scm
+++ languages/bitbake/injections.scm
@@ -2,12 +2,12 @@
   function: (attribute
               object: (python_identifier) @_re)
   arguments: (argument_list (python_string
-                              (string_content) @content) @_string)
+                              (string_content) @injection.content) @_string)
   (#eq? @_re "re")
   (#lua-match? @_string "^r.*")
   (#set! language "regex"))
 
-((shell_content) @content
+((shell_content) @injection.content
   (#set! language "bash"))
 
 ((comment) @injection.content
